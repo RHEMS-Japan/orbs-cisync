28f3206e20e270cf5526e5132839027df51b8186
shindex-rhems <shindex@rhems-japan.co.jp>
2021-11-19 09:51:20 +0900

    fix

--- src/scripts/cisync.sh
+++ src/scripts/cisync.sh
@@ -1,33 +1,30 @@
 Cisync() {
-    MERGE_FROM=$(eval echo "$CIRCLE_BRANCH")
-    if [ "${MERGE_TO}" = "ALL" ]; then
+    merge_from=$(eval echo "${MERGE_FROM}")
+    merge_to=${MERGE_TO}
+    if [ "${merge_to}" = "ALL" ]; then
         git branch -r | grep -v '\->' | while read -r remote; do
             git branch --track "${remote#origin/}" "$remote" || true
         done
         git fetch --all
-        MERGE_TO=$(git for-each-ref --format="%(refname:short)" refs/heads/ | grep -v "${MERGE_FROM}")
+        merge_to=$(git for-each-ref --format="%(refname:short)" refs/heads/ | grep -v "${merge_from}")
     fi
 
     echo "===="
-    echo "from: ${MERGE_FROM}"
-    echo "  to: ${MERGE_TO}"
+    echo "from: ${merge_from}"
+    echo "  to: ${merge_to}"
     echo "===="
-
-    # FOR LOCAL
-    # MERGE_FROM="alpha"
-    # MERGE_TO=$(git for-each-ref --format="%(refname:short)" refs/heads/ | grep -v "${MERGE_FROM}")
 
     cp -Rp .circleci ../
     git config --global user.name "${USER_NAME}"
     git config --global user.email "${USER_EMAIL}"
 
-    for _sync_branch in ${MERGE_TO}; do
+    for _sync_branch in ${merge_to}; do
         echo "${_sync_branch}"
         git checkout "${_sync_branch}"
         rm -rf .circleci
         cp -Rp ../.circleci ./
         git add .circleci
-        git diff-index --quiet HEAD || git commit -m "[skip ci] cisync auto merge from ${MERGE_FROM} -> ${_sync_branch}"
+        git diff-index --quiet HEAD || git commit -m "[skip ci] cisync auto merge from ${merge_from} -> ${_sync_branch}"
         git push
     done
 
